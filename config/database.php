0d0fe66ba405526ac40132b177e1b01397cca3e8
Taneem69 <tanvirtaneem32@gmail.com>
2026-05-11 01:17:26 +0600

    added database connection config file

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+
+    $host="127.0.0.1";
+    $user="root";
+    $password="";
+    $name="online food blog";
+
+
+    function connection(){
+        global $host;
+        global $user;
+        global $password;
+        global $name;
+
+
+        $con=mysqli_connect($host, $user, $password, $name);
+
+
+        if($con){
+            echo "connection success";
+        }
+
+        else{
+            echo"failed";
+        }
+
+    }
+?>
