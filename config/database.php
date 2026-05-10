808ef5789c8f4fbea4ae5234f37a0ffca9da5603
Taneem69 <tanvirtaneem32@gmail.com>
2026-05-11 02:11:55 +0600

    slight change in config/database.php

--- config/database.php
+++ config/database.php
@@ -1,28 +1,18 @@
 <?php
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$dbname = "online food blog";
 
-    $host="127.0.0.1";
-    $user="root";
-    $password="";
-    $name="online food blog";
-
-
-    function connection(){
-        global $host;
-        global $user;
-        global $password;
-        global $name;
-
-
-        $con=mysqli_connect($host, $user, $password, $name);
-
-
-        if($con){
-            return true;
-        }
-
-        else{
-            return false;
-        }
-
+function connection() {
+    global $host, $user, $password, $dbname;
+    $con = mysqli_connect($host, $user, $password, $dbname);
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
     }
+    mysqli_set_charset($con, "utf8mb4");
+    return $con;
+}
+
+$conn = connection();  
 ?>
