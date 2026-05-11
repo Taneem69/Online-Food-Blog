fa466385e5fc7cdc490c5fa160ca57d971311bad
Taneem69 <tanvirtaneem32@gmail.com>
2026-05-12 01:22:29 +0600

    editted database.php file

--- config/database.php
+++ config/database.php
@@ -1,18 +1,14 @@
 <?php
+
 $host = "127.0.0.1";
 $user = "root";
-$password = "";
-$dbname = "online food blog";
+$pass = "";
+$db = "online food blog";
 
-function connection() {
-    global $host, $user, $password, $dbname;
-    $con = mysqli_connect($host, $user, $password, $dbname);
-    if (!$con) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    mysqli_set_charset($con, "utf8mb4");
-    return $con;
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if(!$conn){
+    die("Database Connection Failed");
 }
 
-$conn = connection();  
 ?>
