1f9bd359e741805e269151eb094c451737be7906
Taneem69 <tanvirtaneem32@gmail.com>
2026-05-12 02:11:27 +0600

    added view/food_experience.php and related css and js files

---
+++ asset/food_experience.js
@@ -0,0 +1,57 @@
+function addComment(postId){
+        let comment =
+        document.getElementById('comment'+postId).value;
+
+        if(comment.trim()==""){
+            alert("Comment Required");
+            return;
+        }
+
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST", "../controller/addComment.php",true);
+
+        xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+        xhttp.onreadystatechange = function(){
+
+            if(this.readyState == 4 &&
+            this.status == 200){
+
+                let data =
+                JSON.parse(this.responseText);
+
+                if(data.status=="success"){
+                    location.reload();
+                }
+                else{
+                    alert(data.message);
+                }
+            }
+        }
+
+        xhttp.send("post_id="+postId+"&comment="+encodeURIComponent(comment));
+    }
+
+    function deleteComment(id){
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST","../controller/deleteComment.php",true);
+
+        xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+        xhttp.onreadystatechange = function(){
+
+            if(this.readyState == 4 && this.status == 200){
+
+                let data =
+                JSON.parse(this.responseText);
+
+                if(data.status=="success"){
+                    location.reload();
+                }
+            }
+        }
+
+        xhttp.send("id="+id);
+    }
