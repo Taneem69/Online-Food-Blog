fc2a0a62bee0fbd51670f8248e6d9b58a23593d3
monibur-emu <moniburrahmanemu@gmail.com>
2026-05-13 02:15:30 +0600

    add header footer error page and home amd login page

---
+++ view/partials/404.php
@@ -0,0 +1,7 @@
+<?php require_once __DIR__ . '/header.php'; ?>
+<div class="empty-state" style="text-align:center;padding:80px 20px">
+    <h1 style="font-size:4rem">404</h1>
+    <p>Page not found.</p>
+    <a href="index.php" class="btn-primary">Go Home</a>
+</div>
+<?php require_once __DIR__ . '/footer.php'; ?>
