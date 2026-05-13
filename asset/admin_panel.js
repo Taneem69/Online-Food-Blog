054ce57a4747e6c6c2b1a3718ca8d1cd5e742886
Taneem69 <tanvirtaneem32@gmail.com>
2026-05-13 15:07:03 +0600

    added admin_panel js and php file

---
+++ asset/admin_panel.js
@@ -0,0 +1,44 @@
+function deleteMember(id)
+{
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", "../controller/deleteMember.php", true);
+
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+
+            let data =JSON.parse(this.responseText);
+
+            if(data.status=="success"){
+                location.reload();
+            }
+        }
+    }
+
+    xhttp.send("id="+id);
+}
+
+function deleteReview(id){
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", "../controller/deleteReview.php", true);
+
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+
+            let data = JSON.parse(this.responseText);
+
+            if(data.status=="success"){
+                location.reload();
+            }
+        }
+    }
+
+    xhttp.send("id="+id);
+}
