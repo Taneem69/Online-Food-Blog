ac33f0c77fa5b53a96d6fa23618435e7b4cd5e38
Taneem69 <tanvirtaneem32@gmail.com>
2026-05-16 01:00:34 +0600

    fixed database.php in cofig folder

--- config/database.php
+++ config/database.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = "127.0.0.1";
-$user = "root";
-$pass = "";
-$db = "online food blog";
+    $host = "127.0.0.1";
+    $user = "root";
+    $pass = "";
+    $db = "online food blog";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+    $conn = mysqli_connect($host, $user, $pass, $db);
 
-if(!$conn){
-    die("Database Connection Failed");
-}
+    if(!$conn){
+        die("Database Connection Failed");
+    }
 
+    mysqli_set_charset($conn, 'utf8mb4');
+    
 ?>
